083d16a7a03f2ddc7f286a0b751516aa8e3d0a3c
keyang xiang <keyang.xiang@feedhenry.com>
2012-02-24 16:16:01 +0000

    add init

---
+++ client/default/js/init.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	importViews(function() {
+		changeView("mainPage");
+		var mainPageView = getView("mainPage");
+		mainPageView.find("#info").html("Loading User Data....");
+		users.load(function() {
+			mainPageView.find("#info").html("User Data Loaded!");
+			mainPageView.find("#loginBtn").removeAttr("disabled");
+		});
+	});
+});
